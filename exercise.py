9701d45437472b13d5122881d4609cdac9ca410f
Kasten33 <hurlrykaden@gmail.com>
2024-09-23 13:07:35 -0600

    done?

---
+++ exercise.py
@@ -0,0 +1,4 @@
+myName = "Kaden"
+myAge = 19
+
+print("My name is", myName, "and I am", myAge, "years old.")
---
+++ madlib.py
@@ -0,0 +1,5 @@
+noun = input("Fish, Cat, Dog")
+verb = input("Jumped, Ran, Swam")
+plural_noun = input("Kinfes, water, finish line")
+
+print(f"The {noun} {verb} over the {plural_noun}")
